5216def3827a926a9b6c8646a732fbab465ee82d
diegodevweb <diego.devwebb@gmail.com>
2026-04-28 18:45:13 -0300

    feat: implement error handling in incident.service

--- backend/src/incidents/incidents.controller.ts
+++ backend/src/incidents/incidents.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { IncidentsService } from './incidents.service';
 import { CreateErrorLogDto } from './dto/create-error-log.dto';
 
@@ -9,5 +9,15 @@ export class IncidentsController {
     @Post('error-logs')
     createErrorLog(@Body() body: CreateErrorLogDto) {
         return this.incidentsService.createErrorLog(body);
+    }
+
+    @Get('error-log/:id')
+    getErrorLog(@Param('id', ParseIntPipe) id: number) {
+        return this.incidentsService.getErrorLog(id);
+    }
+
+    @Get('error-logs')
+    getErrorLogs() {
+        return this.incidentsService.getErrorLogs();
     }
 }
--- backend/src/incidents/incidents.service.ts
+++ backend/src/incidents/incidents.service.ts
@@ -1,10 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateErrorLogDto } from './dto/create-error-log.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class IncidentsService {
     constructor(private readonly prisma: PrismaService) {}
+
+    async getErrorLogs() {
+        const incidentList = await this.prisma.incidentLog.findMany();
+
+        if (!incidentList || incidentList.length === 0) {
+            throw new NotFoundException('Nenhum registro encontrado');
+        }
+
+        return incidentList;
+    }
+
+    async getErrorLog(id: number) {
+        const incidentLog = await this.prisma.incidentLog.findUnique({
+            where: { id },
+        });
+
+        if (!incidentLog) {
+            throw new NotFoundException(`Registro não encontrado para o ID ${id}`);
+        }
+
+        return incidentLog;
+    }
 
     async createErrorLog(data: CreateErrorLogDto) {
         const incidentLog = await this.prisma.incidentLog.create({
